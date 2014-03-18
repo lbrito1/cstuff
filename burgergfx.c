083edaca8a354435b47d801d97a9918b9b0df160
Leonardo Brito <lbrito@gmail.com>
2014-03-17 23:49:10 -0300

    s

--- burgergfx.c
+++ burgergfx.c
@@ -1,3 +1,23 @@
+/*
+    File: burgergfx.c
+
+    Copyright (c) 2014 Leonardo Brito <lbrito@gmail.com>
+
+    This software is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write the Free Software Foundation, Inc., 51
+    Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -8,6 +28,13 @@ typedef struct
       char* burger_matrix;
 } burger;
 
+/**
+ *  @brief Create a new w x h burger
+ *  
+ *  @param [in] w width
+ *  @param [in] h height
+ *  @return pointer to burger
+ */
 burger* create(int w, int h)
 {
       burger* bgfx = malloc(sizeof(burger));
@@ -19,22 +46,50 @@ burger* create(int w, int h)
       return bgfx;
 }
 
+/**
+ *  @brief Get the appropriate cell from a normalized x coordinate
+ *  
+ *  @param [in] b burger
+ *  @param [in] x normalized coordinate
+ *  @return cell index
+ */
 int get_norm_x(burger* b, double x)
 {
       return (int) round(b->w*x);
 }
 
+/**
+ *  @brief Get the appropriate cell from a normalized y coordinate
+ *  
+ *  @param [in] b burger
+ *  @param [in] y normalized coordinate
+ *  @return cell index
+ */
 int get_norm_y(burger* b, double y)
 {
       return (int) round(b->h*y);
 }
 
+/**
+ *  @brief Fill the burger with whitespace
+ *  
+ *  @param [in] b 
+
+ */
 void clean_burger(burger* b)
 {
       int i=0;
       for (;i<b->w*b->h;i++) b->burger_matrix[i] = ' ';
 }
 
+/**
+ *  @brief Put char in cell
+ *  
+ *  @param [in] b  burger
+ *  @param [in] dx normalized x position
+ *  @param [in] dy normalized y position
+ *  @param [in] c  char to be put
+ */
 void put_burger(burger* b, double dx, double dy, char c)
 {
       int x = get_norm_x(b, dx);
@@ -42,11 +97,24 @@ void put_burger(burger* b, double dx, double dy, char c)
       b->burger_matrix[y+(x*b->w)] = c;
 }
 
+/**
+ *  @brief Put char in cell
+ *  
+ *  @param [in] b burger
+ *  @param [in] x cell index
+ *  @param [in] y cell index
+ *  @param [in] c char to be put
+ */
 void put_burger_int(burger* b, int x, int y, char c)
 {
       b->burger_matrix[y+(x*b->w)] = c;
 }
 
+/**
+ *  @brief Print the current burger
+ *  
+ *  @param [in] bgfx 
+ */
 void print_burger(burger* bgfx)
 {
       
@@ -64,6 +132,15 @@ void print_burger(burger* bgfx)
       }
 }
 
+/**
+ *  @brief Draw line from point 1 to point 2 on burger
+ *  
+ *  @param [in] bgfx 
+ *  @param [in] dx1  point 1 normalized x coord
+ *  @param [in] dy1  point 1 normalized y coord
+ *  @param [in] dx2  point 2 normalized x coord
+ *  @param [in] dy2  point 2 normalized y coord
+ */
 void put_line(burger* bgfx, double dx1, double dy1, double dx2, double dy2)
 {
       int x1 = get_norm_x(bgfx, dx1);
