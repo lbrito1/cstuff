e73493aecb413c29c6af3f9d20d48813c3c194ff
Leonardo Brito <lbrito@gmail.com>
2014-03-18 18:16:25 -0300

    rm

--- color.h
+++
@@ -1,7 +0,0 @@
-#define COL_RED     "\x1b[31m"
-#define COL_GREEN   "\x1b[32m"
-#define COL_YELLOW  "\x1b[33m"
-#define COL_BLUE    "\x1b[34m"
-#define COL_MAGENTA "\x1b[35m"
-#define COL_CYAN    "\x1b[36m"
-#define COL_RESET   "\x1b[0m"
