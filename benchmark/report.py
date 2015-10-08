14d144a391aab2abccb59e4214626966f1413135
Leonardo <lbrito@gmail.com>
2015-10-08 16:07:29 -0300

    Adding some more plots to tree benchmark

--- benchmark/report.py
+++ benchmark/report.py
@@ -11,7 +11,7 @@ def plot_stuff(name, df, log=False):
     plt.clf()
     xaxis = df.index
     fig, ax = plt.subplots()
-    ax.plot(xaxis, df['bst_insert'], 'o', alpha=0.8, label='bst_insert')
+    # ax.plot(xaxis, df['bst_insert'], 'o', alpha=0.8, label='bst_insert')
     ax.plot(xaxis, df['avl_insert'], 'o', alpha=0.8, label='avl_insert')
     ax.plot(xaxis, df['rb_insert'], 'o', alpha=0.8, label='rb_insert')
     if log:
@@ -25,7 +25,7 @@ def plot_stuff(name, df, log=False):
 
     fig, ax = plt.subplots()
     fig.suptitle(name, fontsize=22)
-    ax.plot(xaxis, df['bst_search'], 'o', alpha=0.8, label='bst_search')
+    # ax.plot(xaxis, df['bst_search'], 'o', alpha=0.8, label='bst_search')
     ax.plot(xaxis, df['avl_search'], 'o', alpha=0.8, label='avl_search')
     ax.plot(xaxis, df['rb_search'], 'o', alpha=0.8, label='rb_search')
     if log: plt.yscale('log')
@@ -44,10 +44,21 @@ def plot_stuff(name, df, log=False):
     plt.savefig('img/'+name+'_height.png', bbox_inches='tight', dpi=300)
     plt.clf()
 
+def plot_height(df):
+    fig, ax = plt.subplots()
+    xaxis = df.index
+    ax.plot(xaxis, df['avl_height'], '-', alpha=0.8, label='avl_height', lw=4)
+    ax.plot(xaxis, df['rb_height'], '-', alpha=0.8, label='rb_height', lw=4)
+    fig.suptitle('Worst case heights', fontsize=22)
+    ax.legend(loc='best', fontsize='large', frameon=True)
+    plt.savefig('img/worst_heights.png', bbox_inches='tight', dpi=300)
+    plt.clf()
 
-df = pd.DataFrame.from_csv("tree_performance.csv",sep=',')
-plot_stuff('Average case', df)
+
+# df = pd.DataFrame.from_csv("tree_performance.csv",sep=',')
+# plot_stuff('Average case', df)
 
 df = pd.DataFrame.from_csv("tree_performance_worst.csv",sep=',')
 plot_stuff('Worst case', df)
-plot_stuff('Worst case', df, True)
+# plot_stuff('Worst case', df, True)
+plot_height(df)
